fe3c6e49ee7f4692f5ce26698ffc97d8e94b8200
Andre <andremt2003@gmail.co>
2023-02-13 10:46:26 -0600

    stack

--- FASE 2/fase 2/src/Stack.java
+++ FASE 2/fase 2/src/Stack.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * FASE 2 PROYECTO 1
  * ALGORITMOS Y ESTRUCTURA DE DATOS
@@ -5,5 +8,29 @@
  * ANDY FUENTES
  * GABRILE PAZ
  */
-public class Stack {
+public class Stack<String> {
+
+    public List<String> elementos;
+
+    public Stack () {
+        this.elementos = new ArrayList<>();
+    }
+
+    public String pop() {
+        return elementos.remove(0);
+    }
+
+    public void push(String item) {
+        elementos.add(item);
+    }
+
+    public String peek() {
+        return elementos.get(0);
+    }
+
+    public boolean empty() {
+        return (elementos.size() == 0);
+    }
+
+
 }
