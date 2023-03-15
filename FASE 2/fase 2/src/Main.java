b2ccd4742b723c217166aaee42a381e3e7c0d5b1
Andre <andremt2003@gmail.co>
2023-03-15 15:32:22 -0600

    switch arreglado

--- FASE 2/fase 2/src/Main.java
+++ FASE 2/fase 2/src/Main.java
@@ -21,52 +21,85 @@ public static void main(String[] args) {
         System.out.println("Ingrese una opcion: ");
         int opcion = sc.nextInt();
         sc.nextLine();
-        switch(opcion){
-            /**
-             * Calculadora arimetrica
-             */
-            case 1:
-                try {
-                    /**
-                     * Se lee el archivo arimetrica.txt
-                     */
-                    BufferedReader reader = new BufferedReader(new FileReader("arimetrica.txt"));
-                    String line;
-                    int cont = 1;
-                    System.out.println("\nEstas son las operaciones que se realizaran con su respectivo resultado:\n");
-                    while ((line = reader.readLine()) != null) {
-                        Transformador TR = new Transformador(line);
-                        double result = TR.evaluate();
-                        System.out.println("Operacion: "+ cont+"\t"+ line + " = " + result);
-                        cont++;
+        while(opcion != 3) {
+            switch (opcion) {
+                /**
+                 * Calculadora arimetrica
+                 */
+                case 1:
+                    try {
+                        /**
+                         * Se lee el archivo arimetrica.txt
+                         */
+                        BufferedReader reader = new BufferedReader(new FileReader("arimetrica.txt"));
+                        String line;
+                        int cont = 1;
+                        System.out.println("\nEstas son las operaciones que se realizaran con su respectivo resultado:\n");
+                        while ((line = reader.readLine()) != null) {
+                            Transformador TR = new Transformador(line);
+                            double result = TR.evaluate();
+                            System.out.println("Operacion: " + cont + "\t" + line + " = " + result);
+                            cont++;
+                        }
+                        reader.close();
+                    } catch (IOException e) {
+                        System.out.println("Error al leer el archivo: " + e.getMessage());
                     }
-                    reader.close();
-                } catch (IOException e) {
-                    System.out.println("Error al leer el archivo: " + e.getMessage());
-                }
-                break;
+                    break;
 
 
-            case 2:
-                /**
-                 * Funciones Fib,Fac,Ftoc
-                 */
-                System.out.println("Ingrese una expresión en el formato '(factorial n)': ");
-                String input = sc.nextLine();
-                Tokenizer tokenizer = new Tokenizer(input);
-                tokenizer.calculateFactorial();
-                break;
-            case 3:
-                /**
-                 * Salir
-                 */
-                System.out.println("Gracias por usar el programa");
-                break;
+                case 2:
+                    /**
+                     * Funciones Fib,Fac,Ftoc
+                     */
+                    String menu2 = "1. Funcion Fib \n2. Funcion Fac \n3. Funcion Ftoc";
+                    System.out.println(menu2);
+                    System.out.println("Ingrese una opcion: ");
+                    int opcion2 = sc.nextInt();
+                    sc.nextLine();
+                    switch (opcion2) {
+                        case 1:
+                            /**
+                             * Funcion Fib
+                             */
+                            break;
+                        case 2:
+                            /**
+                             * Funcion Fac
+                             */
+                            System.out.println("Ingrese una expresión en el formato '(factorial n)': ");
+                            String input2 = sc.nextLine();
+                            Tokenizer tokenizer2 = new Tokenizer(input2);
+                            tokenizer2.calculateFactorial();
+                            break;
+                        case 3:
+                            /**
+                             * Funcion Ftoc
+                             */
+                            break;
 
-            default:
-                System.out.println("Opcion no valida");
-                break;
+                        default:
+                            System.out.println("Opcion no valida");
+                            break;
+                    }
+
+
+                case 3:
+                    /**
+                     * Salir
+                     */
+                    System.out.println("Gracias por usar el programa");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+
+            System.out.println(menu);
+            System.out.println("Ingrese una opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine();
         }
-
     }
 }
