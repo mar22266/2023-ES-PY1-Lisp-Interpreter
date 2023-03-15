7c3b5f40e8ea4c7340ece65daca2bc78a11c6330
Andre <andremt2003@gmail.co>
2023-03-15 10:01:49 -0600

    Cambios

--- FASE 2/fase 2/src/Main.java
+++ FASE 2/fase 2/src/Main.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * GABRILE PAZ
  */
 public class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("\n------Bienvenido al interprete de LISP------\n");
