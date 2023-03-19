098b3f107f8c9b3b2360f825dd7a8704e926b3d3
Andre <andremt2003@gmail.co>
2023-03-19 11:43:46 -0600

    programa casi listo (funciones?)

--- FASE 2/fase 2/src/Main.java
+++ FASE 2/fase 2/src/Main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 /**
  * FASE 2 PROYECTO 1
@@ -8,4 +8,35 @@
  * GABRILE PAZ
  */
 public class Main {
-public static void main(String[] args) {}}
+public static void main(String[] args) {
+
+
+    Lisp lisp = new Lisp();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("BIENVENIDO A LISP");
+    System.out.println("Ingrese EXIT para salir");
+    boolean flag = false;
+    while (flag == false) {
+        String code = "";
+        Boolean flag2 = false;
+        System.out.println("--");
+        while (flag2 == false) {
+            code += sc.nextLine();
+            flag2 = lisp.VerifyPa(code);
+            if (!flag2) {
+                code += " ";
+            }
+        }
+        if (code.equalsIgnoreCase("EXIT")){
+            flag = true;
+        }
+        else {
+            lisp.Translate(code);
+        }
+    }
+}
+
+
+
+
+}
